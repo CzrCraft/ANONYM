3cbe81f5a99ac72d51449ece99dd868d9bb2d64e
CzrCraft <czrcraft@gmail.com>
2023-04-23 23:17:25 +0300

    added login page, not complete yet

--- App/lib/loginPage.dart
+++ App/lib/loginPage.dart
@@ -1,11 +1,178 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:Stylr/main.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/services.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  int animationStage = 0;
+  String username = "";
+  String password = "";
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (animationStage == 0) {
+      Future.delayed(const Duration(milliseconds: 455), () {
+        setState(() {
+          animationStage = 1;
+        });
+      });
+    } else if (animationStage == 1) {
+      Future.delayed(const Duration(milliseconds: 1100), () {
+        setState(() {
+          animationStage = 2;
+        });
+      });
+    }
+
+    switch (animationStage) {
+      case 1:
+        return Container(
+          color: primaryColor,
+          child: Column(
+            children: [
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText("Okay, what is your name?",
+                      speed: const Duration(milliseconds: 45),
+                      textStyle: TextStyle(
+                          fontSize: 25.0,
+                          color: secondaryColor,
+                          decoration: TextDecoration.none)),
+                ],
+                totalRepeatCount: 1,
+                onFinished: () {
+                  setState(() {
+                    animationStage = 2;
+                  });
+                },
+                repeatForever: false,
+                pause: const Duration(milliseconds: 1100),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        );
+        break;
+      case 2:
+        return Container(
+          color: primaryColor,
+          child: Column(
+            children: [
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText("Okay, what is your name?",
+                      speed: const Duration(milliseconds: 45),
+                      textStyle: TextStyle(
+                          fontSize: 25.0,
+                          color: secondaryColor,
+                          decoration: TextDecoration.none)),
+                ],
+                totalRepeatCount: 1,
+                onFinished: () {
+                  setState(() {
+                    animationStage = 2;
+                  });
+                },
+                repeatForever: false,
+                pause: const Duration(milliseconds: 1100),
+              ),
+              Padding(
+                  child: Stack(children: [
+                    Material(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          border: InputBorder.none,
+                          fillColor: primaryColor,
+                          hoverColor: secondaryColor,
+                          focusColor: secondaryColor,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                        ),
+                        textAlignVertical: TextAlignVertical.bottom,
+                        textAlign: TextAlign.center,
+                        cursorColor: secondaryColor,
+                        maxLength: 20,
+                        onSubmitted: (String input) {
+                          input = input.trim();
+                          if (input != null && input != "") {
+                            setState(() {
+                              username = input;
+                              animationStage = 3;
+                            });
+                          }
+                        },
+                        buildCounter: (
+                          BuildContext context, {
+                          required int currentLength,
+                          int? maxLength,
+                          required bool isFocused,
+                        }) =>
+                            null,
+                        style: TextStyle(
+                            backgroundColor: primaryColor,
+                            color: secondaryColor,
+                            fontSize: 25),
+                        showCursor: false,
+                      ),
+                    ),
+                    Positioned(
+                        child: AnimatedTextKit(
+                          animatedTexts: [
+                            TyperAnimatedText("........................",
+                                speed: const Duration(milliseconds: 45),
+                                textStyle: TextStyle(
+                                    fontSize: 25.0,
+                                    color: secondaryColor,
+                                    decoration: TextDecoration.none,
+                                    backgroundColor: Colors.transparent)),
+                          ],
+                          totalRepeatCount: 1,
+                          onFinished: () {
+                            setState(() {
+                              animationStage = 2;
+                            });
+                          },
+                          repeatForever: false,
+                          pause: const Duration(milliseconds: 1100),
+                        ),
+                        top: 30,
+                        left: 20)
+                  ]),
+                  padding: EdgeInsets.only(top: 15)),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        );
+        break;
+      case 3:
+        return Container(child: Center(child: AnimatedTextKit(
+                          animatedTexts: [
+                            TyperAnimatedText("Hello " + username,
+                                speed: const Duration(milliseconds: 45),
+                                textStyle: TextStyle(
+                                    fontSize: 25.0,
+                                    color: secondaryColor,
+                                    decoration: TextDecoration.none,
+                                    backgroundColor: Colors.transparent)),
+                          ],
+                          totalRepeatCount: 1,
+
+                          repeatForever: false,
+                          pause: const Duration(milliseconds: 1100),
+                        )), color: primaryColor);
+        break;
+    }
+    return Container(color: primaryColor);
   }
 }
--- App/lib/main.dart
+++ App/lib/main.dart
@@ -7,17 +7,75 @@ import 'package:flutter/material.dart';
 GlobalKey RootWidgetKey = GlobalKey();
 Color primaryColor = const Color(0xff272324);
 Color secondaryColor = const Color(0xfffeba57);
-void main() {
-  runApp(RootWidget(key: RootWidgetKey));
+
+class CircleRevealClipper extends CustomClipper<Path> {
+  final center;
+  final radius;
+
+  CircleRevealClipper({this.center, this.radius});
+
+  @override
+  Path getClip(Size size) {
+    return Path()..addOval(Rect.fromCircle(radius: radius, center: center));
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
 }
 
-Route _createRoute(Widget page2) {
+Route animatedRoute(Widget p_widget) {
   return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => page2,
+    transitionDuration: Duration(milliseconds: 650),
+    reverseTransitionDuration: Duration(milliseconds: 450),
+    opaque: false,
+    barrierDismissible: false,
+    pageBuilder: (context, animation, secondaryAnimation) => p_widget,
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      var screenSize = MediaQuery.of(context).size;
+      Offset center = Offset(screenSize.width/2, screenSize.height/2);
+      double beginRadius = 0.0;
+      double endRadius = screenSize.height * 1.2;
+
+      var tween = Tween<double>(begin: beginRadius, end: endRadius);
+      double radiusTweenAnimation = animation.drive<double>(tween).value;
+
+      return ClipPath(
+        clipper:
+            CircleRevealClipper(radius: radiusTweenAnimation, center: center),
+        child: child,
+      );
     },
   );
+}
+
+// Route animatedRoute(Widget widget) {
+//   return PageRouteBuilder(
+//     pageBuilder: (context, animation, secondaryAnimatn) => widget,
+//     transitionDuration: Duration(milliseconds: 1000),
+//     opaque: false,
+//     barrierDismissible: false,
+//     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+//       var screenSize = MediaQuery.of(context).size;
+//       Offset center = Offset(screenSize.width - 40, screenSize.height - 40);
+//       double beginRadius = 0.0;
+//       double endRadius = screenSize.height * 1.2;
+
+//       var tween = Tween<double>(begin: beginRadius, end: endRadius);
+//       var radiusTweenAnimation = animation.drive(tween);
+
+//       return ClipPath(
+//         clipper:
+//             CircleRevealClipper(radius: radiusTweenAnimation, center: center),
+//         child: child,
+//       );
+//     },
+//   );
+// }
+
+void main() {
+  runApp(RootWidget(key: RootWidgetKey));
 }
 
 class RootWidget extends StatelessWidget {
@@ -26,11 +84,13 @@ class RootWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Stylr', initialRoute: "/", routes: {
+    return MaterialApp(
+      title: 'Stylr',
+      initialRoute: "/",
+      routes: {
         "/": (context) => const StartPage(),
         "/login": (context) => const LoginPage(),
       },
-      
     );
   }
 }
